3b5fd7440a62ddcfa4832d65f5956fd1073d9d13
chonichen <wchen98@bfx2.win.ad.jhu.edu>
2019-06-04 13:54:28 -0400

    Question 4 answer

---
+++ week01/q4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+
+# A script to copy HTML and CGI files from a project directory to a web
+# serevr directory. 
+# Wanyi Chen wchen98@jh.edu
+# June 4th, 2019
+
+cp -R ~/my_project/html ~/public_html/html
+cp -R ~/my_project/cgi ~/public_html/cgi
