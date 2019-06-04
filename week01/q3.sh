9ee31a6459a7fef886e41d7a422f8e38ed6735d9
chonichen <wchen98@bfx2.win.ad.jhu.edu>
2019-06-04 13:32:29 -0400

    Modify question 3 answer

--- week01/q3.sh
+++ week01/q3.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # A script to setup a bioinformatics project folder
-# Wanyi Chen wchen54@dons.usfca.edu
-# September 9th, 2017
+# Wanyi Chen wchen98@jh.edu
+# June 4th, 2019
 
 # Setup folder structure
 echo "Creating folder structure..."
 mkdir code data data/raw_data output output/figures output/tables
 
+# Download datafiles and processed with the raw data and export processed
+# data to the data diretory.
 echo "Downloading data files..."
 cd data/raw_data
-# Download datafiles and processed with the raw data and export processed 
-# data to the data diretory.
 
 cd ..
 cd ..
